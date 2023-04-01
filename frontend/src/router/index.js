bf682487b1cb3ec3d437974673b25f513f8bf590
unknown <mharfemicaroz@gmail.com>
2023-04-01 09:03:18 +0800

    fix bug

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,6 +6,7 @@ import Booking from "../components/main/BookingComponent.vue";
 import Cashier from "../components/main/CashierComponent.vue";
 import Inventory from "../components/main/InventoryComponent.vue";
 import ExpiredPage from "../components/main/ExpiredPage.vue";
+import CalendarVue from "../components/main/CalendarComponent.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
