94955cef8da8afaaa87d5b31f037d1a3c1b313f6
unknown <mharfemicaroz@gmail.com>
2023-04-06 20:59:41 +0800

    change mysql server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bra_db',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Euler143!',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
