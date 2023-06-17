3baf573ce2db7489ceb62ace625204491e7c3d91
unknown <mharfemicaroz@gmail.com>
2023-06-17 17:43:26 +0800

    fix fix bug

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -43,7 +43,7 @@ const router = createRouter({
     {
       path: "/booking",
       name: "booking",
-      component: EmptyPage,     //changed here :) hehehe
+      component: Booking,     
       meta: { 
         requiresAuth: true,
         roles: ["superuser", "reservationist", "frontdesk"]
