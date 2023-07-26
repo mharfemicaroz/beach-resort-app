27c47a1225b92d8c49586a550c1bf7324c0977ca
unknown <mharfemicaroz@gmail.com>
2023-07-26 22:34:48 +0800

    changed version

--- frontend/src/components/common/FooterComponent.vue
+++ frontend/src/components/common/FooterComponent.vue
@@ -60,7 +60,7 @@ import html2canvas from 'html2canvas';
 export default {
     data() {
         return {
-            version: "1.9.1",
+            version: "1.9.5",
             provider: "Web App Solutions",
             pageVisits: 0,
             bugDescription: "",
