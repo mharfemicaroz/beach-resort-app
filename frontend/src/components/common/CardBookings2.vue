ad9c699d8e0c64a9f18b6f62ee7604a502078819
unknown <mharfemicaroz@gmail.com>
2023-09-24 18:02:20 +0800

    fix counter cardbookings

--- frontend/src/components/common/CardBookings2.vue
+++ frontend/src/components/common/CardBookings2.vue
@@ -8,6 +8,324 @@
               class="accordion-button"
               type="button"
               data-bs-toggle="collapse"
+              data-bs-target="#collapsecancel"
+              aria-expanded="true"
+              aria-controls="collapsecancel"
+            >
+              Cancelled Reservations
+              <span
+                v-if="cancelledRoomData.length > 0"
+                class="badge-pill badge-danger"
+                >{{ cancelledRoomData.length }}</span
+              >
+            </button>
+          </h2>
+          <div
+            id="collapsecancel"
+            class="accordion-collapse collapse show"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <div class="col-md-12">
+                <div class="row row-cols-1 row-cols-md-4">
+                  <div
+                    class="col mb-4"
+                    v-for="(item, index) in cancelledRoomData"
+                    :key="item.id"
+                  >
+                    <div
+                      class="card"
+                      style="transition: transform 0.2s ease-in-out"
+                      @click="
+                        $emit(
+                          'click-action',
+                          item.itemID,
+                          item.name,
+                          item.type,
+                          item.price
+                        )
+                      "
+                    >
+                      <div
+                        class="card-header d-flex justify-content-between align-items-center"
+                        :style="{
+                          'background-color':
+                            item.status === 'reserved' && item.isPaid === 'no'
+                              ? '#ef5350'
+                              : item.status === 'reserved' &&
+                                item.isPaid === 'partial'
+                              ? '#5c6bc0'
+                              : item.status === 'checkedin' &&
+                                item.isPaid === 'no'
+                              ? '#66bb6a'
+                              : item.status === 'checkedin' &&
+                                item.isPaid === 'partial'
+                              ? '#42a5f5'
+                              : item.status === 'checkedin' &&
+                                item.isPaid === 'yes'
+                              ? '#ffee58'
+                              : item.status === 'reserved' &&
+                                item.isPaid === 'yes'
+                              ? '#5c6bc0'
+                              : '',
+                        }"
+                      >
+                        <h5 class="card-title">
+                          <i class="fas fa-home"></i> {{ item.name }}
+                        </h5>
+                      </div>
+
+                      <div class="card-body" v-if="'itemID' in item">
+                        <h6>Client Information:</h6>
+                        <ul class="list-unstyled">
+                          <li
+                            v-if="'groupkey' in item && item.groupkey !== null"
+                          >
+                            <strong
+                              ><i class="fas fa-user"></i> Client Name:</strong
+                            ><span class="text-white bg-danger">GROUP</span> -
+                            {{ item.clientName }}
+                          </li>
+                          <li v-else>
+                            <strong
+                              ><i class="fas fa-user"></i> Client Name:</strong
+                            >{{ item.clientName }}
+                          </li>
+                          <li>
+                            <strong
+                              ><i class="fas fa-map-marker-alt"></i> Client
+                              Address:</strong
+                            >
+                            {{ item.clientAddress }}
+                          </li>
+                          <li>
+                            <strong
+                              ><i class="fas fa-phone"></i> Contact
+                              Number:</strong
+                            >
+                            {{ item.contactNumber }}
+                          </li>
+                        </ul>
+                        <h6>Booking Information:</h6>
+                        <ul class="list-unstyled">
+                          <li>
+                            <strong
+                              ><i class="fas fa-calendar-alt"></i> Check-in
+                              Date:</strong
+                            >
+                            {{ item.checkinDate }}
+                          </li>
+                          <li>
+                            <strong
+                              ><i class="fas fa-calendar-check"></i> Check-out
+                              Date:</strong
+                            >
+                            {{ item.checkoutDate2 }}
+                          </li>
+                          <li>
+                            <strong
+                              ><i class="fas fa-dollar-sign"></i> Total
+                              Charges:</strong
+                            >
+                            <span class="text-success">{{
+                              item.totalPrice
+                            }}</span>
+                          </li>
+                          <li>
+                            <strong
+                              ><i class="fas fa-money-bill-wave"></i> Partial
+                              Payment:</strong
+                            >
+                            <span class="text-warning">{{
+                              item.partialPayment
+                            }}</span>
+                          </li>
+                        </ul>
+                      </div>
+
+                      <div class="card-body" v-else>
+                        <div
+                          class="alert alert-warning text-center"
+                          role="alert"
+                        >
+                          <h4 class="alert-heading">
+                            This room is available for booking
+                          </h4>
+                          <p>Please click to continue.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button
+              class="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapsehold"
+              aria-expanded="true"
+              aria-controls="collapsehold"
+            >
+              Temporary Reservations
+              <span
+                v-if="holdRoomData.length > 0"
+                class="badge-pill badge-danger"
+                >{{ holdRoomData.length }}</span
+              >
+            </button>
+          </h2>
+          <div
+            id="collapsehold"
+            class="accordion-collapse collapse show"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <div class="col-md-12">
+                <div class="row row-cols-1 row-cols-md-4">
+                  <div
+                    class="col mb-4"
+                    v-for="(item, index) in holdRoomData"
+                    :key="item.id"
+                  >
+                    <div
+                      class="card"
+                      style="transition: transform 0.2s ease-in-out"
+                      @click="
+                        $emit(
+                          'click-action',
+                          item.itemID,
+                          item.name,
+                          item.type,
+                          item.price
+                        )
+                      "
+                    >
+                      <div
+                        class="card-header d-flex justify-content-between align-items-center"
+                        :style="{
+                          'background-color':
+                            item.status === 'reserved' && item.isPaid === 'no'
+                              ? '#ef5350'
+                              : item.status === 'reserved' &&
+                                item.isPaid === 'partial'
+                              ? '#5c6bc0'
+                              : item.status === 'checkedin' &&
+                                item.isPaid === 'no'
+                              ? '#66bb6a'
+                              : item.status === 'checkedin' &&
+                                item.isPaid === 'partial'
+                              ? '#42a5f5'
+                              : item.status === 'checkedin' &&
+                                item.isPaid === 'yes'
+                              ? '#ffee58'
+                              : item.status === 'reserved' &&
+                                item.isPaid === 'yes'
+                              ? '#5c6bc0'
+                              : '',
+                        }"
+                      >
+                        <h5 class="card-title">
+                          <i class="fas fa-home"></i> {{ item.name }}
+                        </h5>
+                      </div>
+
+                      <div class="card-body" v-if="'itemID' in item">
+                        <h6>Client Information:</h6>
+                        <ul class="list-unstyled">
+                          <li
+                            v-if="'groupkey' in item && item.groupkey !== null"
+                          >
+                            <strong
+                              ><i class="fas fa-user"></i> Client Name:</strong
+                            ><span class="text-white bg-danger">GROUP</span> -
+                            {{ item.clientName }}
+                          </li>
+                          <li v-else>
+                            <strong
+                              ><i class="fas fa-user"></i> Client Name:</strong
+                            >{{ item.clientName }}
+                          </li>
+                          <li>
+                            <strong
+                              ><i class="fas fa-map-marker-alt"></i> Client
+                              Address:</strong
+                            >
+                            {{ item.clientAddress }}
+                          </li>
+                          <li>
+                            <strong
+                              ><i class="fas fa-phone"></i> Contact
+                              Number:</strong
+                            >
+                            {{ item.contactNumber }}
+                          </li>
+                        </ul>
+                        <h6>Booking Information:</h6>
+                        <ul class="list-unstyled">
+                          <li>
+                            <strong
+                              ><i class="fas fa-calendar-alt"></i> Check-in
+                              Date:</strong
+                            >
+                            {{ item.checkinDate }}
+                          </li>
+                          <li>
+                            <strong
+                              ><i class="fas fa-calendar-check"></i> Check-out
+                              Date:</strong
+                            >
+                            {{ item.checkoutDate2 }}
+                          </li>
+                          <li>
+                            <strong
+                              ><i class="fas fa-dollar-sign"></i> Total
+                              Charges:</strong
+                            >
+                            <span class="text-success">{{
+                              item.totalPrice
+                            }}</span>
+                          </li>
+                          <li>
+                            <strong
+                              ><i class="fas fa-money-bill-wave"></i> Partial
+                              Payment:</strong
+                            >
+                            <span class="text-warning">{{
+                              item.partialPayment
+                            }}</span>
+                          </li>
+                        </ul>
+                      </div>
+
+                      <div class="card-body" v-else>
+                        <div
+                          class="alert alert-warning text-center"
+                          role="alert"
+                        >
+                          <h4 class="alert-heading">
+                            This room is available for booking
+                          </h4>
+                          <p>Please click to continue.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button
+              class="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
               data-bs-target="#collapseOne"
               aria-expanded="true"
               aria-controls="collapseOne"
@@ -678,9 +996,46 @@ export default {
     };
   },
   computed: {
+    cancelledRoomData() {
+      return this.roomData
+        .filter((o) => o.status === "cancelled")
+        .map((o) => {
+          let checkoutDate = new Date(parseDate(o.checkoutDate));
+          checkoutDate.setDate(checkoutDate.getDate() + 1);
+          const checkoutDate2 = formatDate(checkoutDate.setHours(0, 0, 0, 0));
+          return {
+            checkoutDate2,
+            ...o,
+          };
+        })
+        .sort(function (a, b) {
+          return a.isPaid.localeCompare(b.isPaid);
+        });
+    },
+    holdRoomData() {
+      return this.roomData
+        .filter(
+          (o) =>
+            o.status === "reserved" && (o.isPaid === "" || o.isPaid === "no")
+        )
+        .map((o) => {
+          let checkoutDate = new Date(parseDate(o.checkoutDate));
+          checkoutDate.setDate(checkoutDate.getDate() + 1);
+          const checkoutDate2 = formatDate(checkoutDate.setHours(0, 0, 0, 0));
+          return {
+            checkoutDate2,
+            ...o,
+          };
+        })
+        .sort(function (a, b) {
+          return a.isPaid.localeCompare(b.isPaid);
+        });
+    },
     arrivingRoomData() {
       return this.roomData
-        .filter((o) => o.status === "reserved")
+        .filter(
+          (o) => o.status === "reserved" && o.isPaid !== "" && o.isPaid !== "no"
+        )
         .map((o) => {
           let checkoutDate = new Date(parseDate(o.checkoutDate));
           checkoutDate.setDate(checkoutDate.getDate() + 1);
@@ -736,7 +1091,12 @@ export default {
         });
     },
     vacantRoomData() {
-      return this.roomData.filter((o) => !("itemID" in o));
+      // Use filter to remove items that exist in this.departRoomData
+      return this.roomData.filter(
+        (o) =>
+          !("itemID" in o) &&
+          !this.departRoomData.some((d) => d.name === o.name)
+      );
     },
   },
 };
